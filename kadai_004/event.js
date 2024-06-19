4f8720255e402d44bab20d2a580d6ffa27e89b92
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-06-19 19:49:25 +0900

    kadai004を再提出

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,10 +1,9 @@
+$(window).on("load",function(){
+    console.log('loadイベントが発生しました')
+});
 $(function() {
-    $(document).on('load scroll', (e) => {
-        if(e.type === 'load'){
-            console.log('loadイベントが発生しました');
-        }
-        if(e.type === 'scroll'){
-            console.log('scrollイベントが発生しました');
-        }
+    $(window).on("scroll",function(){
+        console.log('scrollイベントが発生しました')
     });
 });
+
